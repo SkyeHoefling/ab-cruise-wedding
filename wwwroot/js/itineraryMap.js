2f11d9b637cc3a58f7aa526456da81aac516a897
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-06-02 22:29:14 -0400

    added panning and zooming

--- wwwroot/js/itineraryMap.js
+++ wwwroot/js/itineraryMap.js
@@ -5,12 +5,17 @@ itineraryMap = {
     $nassau: null,
     $map: null,
     $view: null,
+    $zoom: null,
     initialize: function(){
         itineraryMap.$newYork = ol.proj.fromLonLat([-74.072564, 40.665126]);
+        itineraryMap.$florida = ol.proj.fromLonLat([-80.627721, 28.416275]);
+        itineraryMap.$cococay = ol.proj.fromLonLat([-77.938920, 25.819369]);
+        itineraryMap.$nassau = ol.proj.fromLonLat([-77.340213, 25.079547]);
+        itineraryMap.$zoom = 7;
 
         itineraryMap.$view = new ol.View({
             center: itineraryMap.$newYork,
-            zoom: 10
+            zoom: itineraryMap.$zoom
         });
 
         itineraryMap.$map = new ol.Map({
@@ -29,10 +34,18 @@ itineraryMap = {
       $('#map').height(height);
       itineraryMap.$map.updateSize();
     },
-    panMap: function(location){
+    panMap: function(location, zoom){
+        var currentZoom = null;
+        if(zoom != undefined){
+            currentZoom = zoom;
+        } else {
+            currentZoom = itineraryMap.$zoom;
+        }
+
         itineraryMap.$view.animate({
             center: location,
-            duration:3000
+            duration:3000,
+            zoom: currentZoom
         });
     }
 };
