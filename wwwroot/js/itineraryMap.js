7ae392f4f2899280b7243e35db826bc15ce3c61c
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-06-02 21:56:14 -0400

    map now loads on new york

--- wwwroot/js/itineraryMap.js
+++ wwwroot/js/itineraryMap.js
@@ -1,5 +1,27 @@
 itineraryMap = {
+    $newYork: null,
+    $florida: null,
+    $cococay: null,
+    $nassau: null,
+    $map: null,
     initialize: function(){
-        alert('foo');
+        itineraryMap.$newYork = ol.proj.fromLonLat([-74.072564, 40.665126]);
+
+        var view = new ol.View({
+            center: itineraryMap.$newYork,
+            zoom: 10
+        });
+
+        itineraryMap.$map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            preload: 4,
+            source: new ol.source.OSM()
+          })
+        ],
+        loadTilesWhileAnimating: true,
+        view: view
+      });
     }
 };
