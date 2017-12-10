03411df298a841fc9da0eab95cd4659f09cc064d
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-12-09 21:57:55 -0500

    Updated itinerary to have recepiton location and hotel information

--- wwwroot/js/itineraryMap.js
+++ wwwroot/js/itineraryMap.js
@@ -1,20 +1,16 @@
 itineraryMap = {
-    $newYork: null,
-    $florida: null,
-    $cococay: null,
-    $nassau: null,
+    $campBrockway: null,
+    $delLago: null,
     $map: null,
     $view: null,
     $zoom: null,
     initialize: function(){
-        itineraryMap.$newYork = ol.proj.fromLonLat([-74.072564, 40.665126]);
-        itineraryMap.$florida = ol.proj.fromLonLat([-80.627721, 28.416275]);
-        itineraryMap.$cococay = ol.proj.fromLonLat([-77.938920, 25.819369]);
-        itineraryMap.$nassau = ol.proj.fromLonLat([-77.340213, 25.079547]);
-        itineraryMap.$zoom = 7;
+        itineraryMap.$campBrockway = ol.proj.fromLonLat([-75.993466, 42.927426]);
+        itineraryMap.$delLago = ol.proj.fromLonLat([-76.847175, 42.967603]);
+        itineraryMap.$zoom = 15;
 
         itineraryMap.$view = new ol.View({
-            center: itineraryMap.$newYork,
+            center: itineraryMap.$campBrockway,
             zoom: itineraryMap.$zoom
         });
         var vectorLayer = itineraryMap.getMarkers();
@@ -32,7 +28,7 @@ itineraryMap = {
             view: itineraryMap.$view
         });
 
-        var height = $('.itinerary .columns:first').height();
+        var height = $('.itinerary .columns:first').height() * 1.5;
         $('#map').height(height);
         itineraryMap.$map.updateSize();
     },
@@ -40,19 +36,11 @@ itineraryMap = {
         var markers = [
             new ol.Feature({
                 type: 'icon',
-                geometry: new ol.geom.Point(itineraryMap.$newYork)
+                geometry: new ol.geom.Point(itineraryMap.$campBrockway)
             }),
             new ol.Feature({
                 type: 'icon',
-                geometry: new ol.geom.Point(itineraryMap.$florida)
-            }),
-            new ol.Feature({
-                type: 'icon',
-                geometry: new ol.geom.Point(itineraryMap.$cococay)
-            }),
-            new ol.Feature({
-                type: 'icon',
-                geometry: new ol.geom.Point(itineraryMap.$nassau)
+                geometry: new ol.geom.Point(itineraryMap.$delLago)
             })
         ];
 
@@ -66,20 +54,12 @@ itineraryMap = {
     },
     panMap: function(location, zoom){
         var currentZoom = null;
-        var duration= 4000;
+        var duration= 15000;
 
         if(zoom != undefined){
             currentZoom = zoom;
         } else {
             currentZoom = itineraryMap.$zoom;
-        }
-
-        if((itineraryMap.$newYork[0] == itineraryMap.$view.getCenter()[0] &&
-            itineraryMap.$newYork[1] == itineraryMap.$view.getCenter()[1]) ||
-           (itineraryMap.$newYork[0] == location[0] &&
-            itineraryMap.$newYork[1] == location[1]) )
-        {
-            duration = 8000;
         }
 
         itineraryMap.$view.animate({
