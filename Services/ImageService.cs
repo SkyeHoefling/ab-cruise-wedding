897819822daa0e664bc1e95201a09d8b64ed8bff
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-12-23 23:20:39 -0500

    Updated image categories

--- Services/ImageService.cs
+++ Services/ImageService.cs
@@ -17,7 +17,7 @@ namespace ABCruiseWedding.Services
         public async Task<List<String>> GetCategories()
         {
             await Task.Delay(1);
-            return new List<string> { "Wedding", "Engagement", "Group", "Misc" };
+            return new List<string> { "Wedding", "Engagement", "Other" };
         }
 
         public async Task<List<ImageModel>> GetImages(string container)
